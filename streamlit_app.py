64561064a388a6e58017094d9c567e30f37e43e9
Nikoleta Gkagkani <nikoleta.gagani@hotmail.com>
2022-12-02 01:14:11 +0000

    made a base streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,33 @@
 import streamlit as st
 from audio_recorder_streamlit import audio_recorder
+from PIL import Image
 
-def main():
+# Global styles
+st.markdown('<style>.block-container { padding-top: 20px } h2#languini-ai { color: coral; text-align: center; padding-bottom: 0 }</style>', unsafe_allow_html=True)
+
+# App header
+st.header('Languini_AI')
+
+# Nice centered image
+_, image_col, _ = st.columns(3)
+img = Image.open('./img1.jpg')
+with image_col:
+    st.image(img, width=200)
+
+# App Description
+st.markdown('''
+<p style="text-align: center; margin-bottom: 50px;">Languini is an application that does cool stuff.</p>
+''', unsafe_allow_html=True)
+
+# Getting Started
+st.markdown('<h3 style="text-align: center;">Get Started</h3>', unsafe_allow_html=True)
+
+_, audio_recorder_col, _ = st.columns(3)
+
+with audio_recorder_col:
     audio_bytes = audio_recorder()
-    # Do something with audio bytes
-    if audio_bytes:
-        st.audio(audio_bytes, format="audio/wav")
-        st.download_button('Download your voice :)', audio_bytes, 'yourvoice.wav', 'audio/wav')
 
-    # Machine learning code to do something with audio bytes
-
-
-if __name__ == "__main__":
-    main()
+if audio_bytes:
+    st.audio(audio_bytes, format="audio/wav")
+    if st.button('Process audio', 'btn-process-audio', 'This will use ML to analyze your audio'):
+        st.balloons()
