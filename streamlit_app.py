c47bccaa1bb6767cbb3b40157d561608517232c6
Nikoleta Gkagkani <nikoleta.gagani@hotmail.com>
2022-12-04 20:32:38 +0000

    made an api request

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,41 @@
 import streamlit as st
 from audio_recorder_streamlit import audio_recorder
-from PIL import Image
+import numpy as np
+#from PIL import Image
+#import soundfile as sf
+#from scipy.io import wavfile
+import io
+import librosa
+import librosa.display
+from librosa.feature import melspectrogram
+#from scipy import signal
+import matplotlib.pyplot as plt
+#import soundfile as sf
+import requests
 
-# Global styles
-st.markdown('<style>.block-container { padding-top: 20px } h2#languini-ai { color: coral; text-align: center; padding-bottom: 0 }</style>', unsafe_allow_html=True)
-
-# App header
-st.header('Languini_AI')
-
-# Nice centered image
-_, image_col, _ = st.columns(3)
-img = Image.open('./img1.jpg')
-with image_col:
-    st.image(img, width=200)
-
-# App Description
-st.markdown('''
-<p style="text-align: center; margin-bottom: 50px;">Languini is an application that does cool stuff.</p>
-''', unsafe_allow_html=True)
-
-# Getting Started
-st.markdown('<h3 style="text-align: center;">Get Started</h3>', unsafe_allow_html=True)
-
-_, audio_recorder_col, _ = st.columns(3)
-
-with audio_recorder_col:
-    audio_bytes = audio_recorder()
+audio_bytes = audio_recorder()
 
 if audio_bytes:
     st.audio(audio_bytes, format="audio/wav")
-    if st.button('Process audio', 'btn-process-audio', 'This will use ML to analyze your audio'):
-        st.balloons()
+    #sample_rate, samples = wavfile.read(io.BytesIO(audio_bytes))
+    #frequencies, times, spectrogram = signal.spectrogram(samples, sample_rate)
+
+    y, sr = librosa.load(io.BytesIO(audio_bytes))[0], 44100
+    S = librosa.feature.melspectrogram(y=y, sr=sr)
+    S_dB = librosa.power_to_db(S, ref=np.max)
+    plt.figure()
+    librosa.display.specshow(S_dB)
+
+    # plt.savefig("original_sg/s_7.jpg")
+    st.pyplot(plt)
+
+    # plt.pcolormesh(times, frequencies, spectrogram)
+    # plt.imshow(spectrogram)
+    # plt.ylabel('Frequency [Hz]')
+    # plt.xlabel('Time [sec]')
+    # plt.show()
+
+    st.download_button('TEST',audio_bytes,'test.wav')
+   
+    res = requests.post('http://192.168.1.126:8080/pic', files={ 'audio': audio_bytes })
+    st.markdown(res)
