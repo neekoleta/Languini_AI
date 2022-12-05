6cd781172eed7a6f13f79af7a003ef9739b9ecc8
Nikoleta Gkagkani <nikoleta.gagani@hotmail.com>
2022-12-05 01:18:08 +0000

    Progress

---
+++ demo_app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from audio_recorder_streamlit import audio_recorder
+from PIL import Image
+import matplotlib.pyplot as plt
+
+# Global styles
+st.markdown('<style>.block-container { padding-top: 20px } h2#languini-ai { color: coral; text-align: center; padding-bottom: 0 }</style>', unsafe_allow_html=True)
+
+# App header
+st.header('Languini_AI')
+
+# Nice centered image
+_, image_col, _ = st.columns(3)
+img = Image.open('./img1.jpg')
+with image_col:
+    st.image(img, width=100)
+
+# App Description
+st.markdown('''
+<p style="text-align: center; margin-bottom: 50px;">Languini is an application that does cool stuff.</p>
+''', unsafe_allow_html=True)
+
+# Getting Started
+st.markdown('<h3 style="text-align: center;">Get Started</h3>', unsafe_allow_html=True)
+
+_, audio_recorder_col, _ = st.columns(3)
+
+with audio_recorder_col:
+    audio_bytes = audio_recorder()
+
+if audio_bytes:
+    st.audio(audio_bytes, format="audio/wav")
+    if st.button('Process audio', 'btn-process-audio', 'This will use ML to analyze your audio'):
+        st.balloons()
+    #librosa
+
+    st.download_button('not a virus',audio_bytes,'not_a_virus.wav')
+
+
+st.markdown('''
+            <p style="text-align: left; color:blue;">am blue</p>
+            ''',unsafe_allow_html=True)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,41 +1,63 @@
 import streamlit as st
 from audio_recorder_streamlit import audio_recorder
 import numpy as np
-#from PIL import Image
-#import soundfile as sf
-#from scipy.io import wavfile
 import io
-import librosa
 import librosa.display
 from librosa.feature import melspectrogram
-#from scipy import signal
 import matplotlib.pyplot as plt
-#import soundfile as sf
 import requests
+import json
+from PIL import Image
+#from scipy.io import wavfile
+#import soundfile as sf
+#from scipy import signal
 
-audio_bytes = audio_recorder()
+# Global styles
+st.markdown('<style>.block-container { padding-top: 20px } h2#languini-ai { color: coral; text-align: center; padding-bottom: 0 }</style>', unsafe_allow_html=True)
 
+# App header
+st.header('Languini_AI')
+
+# Nice centered image
+_, image_col, _ = st.columns(3)
+img = Image.open('./img1.jpg')
+with image_col:
+    st.image(img, width=220)
+
+# App Description
+st.markdown('''
+<p style="text-align: center; margin-bottom: 50px;">Languini is an application that does cool stuff.</p>
+''', unsafe_allow_html=True)
+
+# Getting Started
+st.markdown('<h3 style="text-align: center;">Get Started</h3>', unsafe_allow_html=True)
+
+# Center the audio recorder (this collapses to the left if screen too small though)
+_, audio_recorder_col, _ = st.columns(3)
+with audio_recorder_col:
+    audio_bytes = audio_recorder()
+
+# audio_bytes will be set once audio_recorder finished recording audio
 if audio_bytes:
+    # Display the audio player
     st.audio(audio_bytes, format="audio/wav")
-    #sample_rate, samples = wavfile.read(io.BytesIO(audio_bytes))
-    #frequencies, times, spectrogram = signal.spectrogram(samples, sample_rate)
-
+    # Convert the raw audio byte array to numpy n-dimensional array
+    # of the audio power, and also the sample rate.
     y, sr = librosa.load(io.BytesIO(audio_bytes))[0], 44100
-    S = librosa.feature.melspectrogram(y=y, sr=sr)
+    # Calculate melspectrogram data of the audio (frequency intensity against time)
+    S = melspectrogram(y=y, sr=sr)
+    # Normalize raw spectrogram data to decibels, making it more presentable.
     S_dB = librosa.power_to_db(S, ref=np.max)
+
     plt.figure()
     librosa.display.specshow(S_dB)
 
-    # plt.savefig("original_sg/s_7.jpg")
     st.pyplot(plt)
+    st.download_button('Download Audio',audio_bytes,'test.wav')
 
-    # plt.pcolormesh(times, frequencies, spectrogram)
-    # plt.imshow(spectrogram)
-    # plt.ylabel('Frequency [Hz]')
-    # plt.xlabel('Time [sec]')
-    # plt.show()
-
-    st.download_button('TEST',audio_bytes,'test.wav')
-   
-    res = requests.post('http://192.168.1.126:8080/pic', files={ 'audio': audio_bytes })
-    st.markdown(res)
+    # Demo functionality showing how to send the audio bytes as a "file"
+    # in a POST request, and how to extract data from the response.
+    if st.button('Send Audio Data'):
+        res = requests.post('http://192.168.1.126:8080/pic', files={ 'audio': audio_bytes })
+        parsed_res_body = json.loads(res.text)
+        st.markdown(parsed_res_body["hello"])
