9b369e515c4af93aff39317925de33afe45e59a5
Nikoleta Gkagkani <nikoleta.gagani@hotmail.com>
2022-12-07 10:25:52 +0000

    streamlit ready to test on the cloud

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,10 +23,10 @@ st.markdown('<style>.block-container { padding-top: 20px } h2#languini-ai { colo
 st.header('Languini_AI')
 
 # Nice centered image
-_, image_col, _= st.columns(3)
-img = Image.open('./img1.jpg')
-with image_col:
-    st.image(img, width=220)
+# _, image_col, _= st.columns(3)
+# img = Image.open('./img1.jpg')
+# with image_col:
+#     st.image(img, width=220)
 
 # App Description
 st.markdown('''
@@ -34,50 +34,40 @@ st.markdown('''
 ''', unsafe_allow_html=True)
 
 # Getting Started
-st.markdown('<h3 style="text-align: center;">Get Started</h3>', unsafe_allow_html=True)
-word=st.selectbox(label='<h4 style="text-align: center;">Type in the word you want to learn</h4>',options=word_array,key='test')
+st.markdown('<h3 style="text-align: center;">Type in the word you want to learn</h3>', unsafe_allow_html=True)
+word=st.selectbox(label='Type in the word you want to learn',options=word_array,key='test',label_visibility='hidden')
 # Center the audio recorder (this collapses to the left if screen too small though)
-
+st.markdown('<div style="height: 50px;"></div>', unsafe_allow_html=True)
 if word != ' ':
 
+    params = {
+        'word' : word
+        , 'user' : 'root'
+    }
+    res = requests.post('https://languiniai-api-okwty2epfq-ew.a.run.app/get_example', params=params, headers = { 'accept': 'application/json' })
+    st.audio(res.content, format='audio/mp3')
+
 # Returns an audio example of the word correctly pronounced
-    _, audio_recorder_col, _ = st.columns(3)
-    with audio_recorder_col:
-        audio_bytes = audio_recorder()
+    if res:
+        col1,col2,col3=st.columns(3)
+        with col2:
+            audio_bytes = audio_recorder()
+            st.markdown('<div style="height: 0px;"></div>', unsafe_allow_html=True)
+        if audio_bytes:
+        # Display the audio player
+            st.audio(audio_bytes, format="audio/wav")
     # audio_bytes will be set once audio_recorder finished recording audio
 
-        if audio_bytes:
-            # Display the audio player
-            st.audio(audio_bytes, format="audio/wav")
 
-            st.download_button('Download Audio',audio_bytes,'test.wav')
-
-        if st.button('Send Data'):
-            params = {
-                'word' : word
-                , 'user' : 'zac'
-            }
-            #https://languiniai-api-okwty2epfq-ew.a.run.app
-            res = requests.post(f'http://10.80.104.18:8000/get_example', params=params, headers = { 'accept': 'application/json' })
-            st.audio(res.content, format='audio/mp3')
-
-            # Demo functionality showing how to send the audio bytes as a "file"
-            # in a POST request, and how to extract data from the response.
-        if st.button('Send Audio Data'):
-            params = {
-                'word' : 'again'
-                , 'user' : 'zac'
-            }
-            res = requests.post(f'http://10.80.104.18:8000/get_result', params=params, files={'file': audio_bytes }, headers = { 'accept': 'application/json' },timeout=15 )
-            parsed_res_body = json.loads(res.text)
-            st.markdown(parsed_res_body['user'])
-
-        # if st.button('Send Audio Data'):
-        #     params = {
-        #         'word' : 'cat'
-        #         , 'user' : 'zach'
-        #     }
-        #     #https://languiniai-api-okwty2epfq-ew.a.run.app
-        #     res = requests.post(f'https://languiniai-api-okwty2epfq-ew.a.run.app/receive_audio', params=params, files={ 'file': audio_bytes }, headers = { 'accept': 'application/json' })
-
-        #     st.audio(res.content, format='audio/mp3')
+        col1,col2,col3=st.columns(3)
+        with col3:
+                # Demo functionality showing how to send the audio bytes as a "file"
+                # in a POST request, and how to extract data from the response.
+            if st.button('Send Audio Data'):
+                params = {
+                    'word' : word
+                    , 'user' : 'niko'
+                }
+                res = requests.post('https://languiniai-api-okwty2epfq-ew.a.run.app/get_result', params=params, files={'file': audio_bytes }, headers = { 'accept': 'application/json' },timeout=15 )
+                parsed_res_body = json.loads(res.text)
+                st.markdown(parsed_res_body['dates'])
